ef1dab42146f750e976be98cf205cd2207d885c1
ASIF KHAN OFC <122696661+Asif-Ofc@users.noreply.github.com>
2023-01-17 11:06:07 +0500

    Add files via upload

--- command/help.js
+++ command/help.js
@@ -15,7 +15,7 @@ function toCommas(x) {
 exports.allMenu = (role, ucapanWaktu, pushname, mundur, upload, download, ownerName, botName, jam, tanggal, runtime, isCreator, isPremium, sender, limitCount, limit, gcount, glimit, balance, prefix) => {
     return`${ucapanWaktu} ${pushname !== undefined ? pushname : 'Kak'}
 
-Hitung Mundur Bulan Ramadhan 🌙 
+Ramadan Countdown 🌙 
 ${mundur}
 
 *STATISTICS*
